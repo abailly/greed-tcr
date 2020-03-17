e7481b230a590dc71585f0dce74a2affa04466b1
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-03-17 20:13:09 +0100

    working

--- test/GreedSpec.hs
+++ test/GreedSpec.hs
@@ -2,25 +2,25 @@ module GreedSpec where
 
 import Test.Hspec
 
-score :: [Int] -> Int
-score [1,1,1] = 1000
-score [a,b,c] 
+scoreUnLancer :: [Int] -> Int
+scoreUnLancer [1,1,1] = 1000
+scoreUnLancer [a,b,c] 
     | a == b && b == c = a * 100
-score (1:rest) = 100 + score rest
-score (5:rest) = 50 + score rest
-score _ = 0
+scoreUnLancer (1:rest) = 100 + scoreUnLancer rest
+scoreUnLancer (5:rest) = 50 + scoreUnLancer rest
+scoreUnLancer _ = 0
 
 -- http://codingdojo.org/kata/Greed/
 spec :: Spec 
 spec = describe "Greed" $ do
 
-    it "A single one (100)" $ score [1] `shouldBe` 100
-    it "A single five (50)" $ score [5] `shouldBe` 50
-    it "A single two (0)" $ score [2] `shouldBe` 0
-    it "two 1s (0)" $ score [1,1] `shouldBe` 200
-    it "triples 1 (1000)" $ score [1, 1, 1] `shouldBe` 1000
-    it "triples 2 (200)" $ score [2, 2, 2] `shouldBe` 200
-    it "triples 3 (300)" $ score [3,3,3] `shouldBe` 300
-    it "single 1 among others (100)" $ score [1,3,6] `shouldBe` 100
-    it "single 1 and then 3 6s (700)" $ score [1,6,6,6] `shouldBe` 700
-    it "two 5s (100)" $ score [5,5] `shouldBe` 100
+    it "A single one (100)" $ scoreUnLancer [1] `shouldBe` 100
+    it "A single five (50)" $ scoreUnLancer [5] `shouldBe` 50
+    it "A single two (0)" $ scoreUnLancer [2] `shouldBe` 0
+    it "two 1s (0)" $ scoreUnLancer [1,1] `shouldBe` 200
+    it "triples 1 (1000)" $ scoreUnLancer [1, 1, 1] `shouldBe` 1000
+    it "triples 2 (200)" $ scoreUnLancer [2, 2, 2] `shouldBe` 200
+    it "triples 3 (300)" $ scoreUnLancer [3,3,3] `shouldBe` 300
+    it "single 1 among others (100)" $ scoreUnLancer [1,3,6] `shouldBe` 100
+    it "single 1 and then 3 6s (700)" $ scoreUnLancer [1,6,6,6] `shouldBe` 700
+    it "two 5s (100)" $ scoreUnLancer [5,5] `shouldBe` 100
