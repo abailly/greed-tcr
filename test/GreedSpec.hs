bdb94a839a06a3c38366b717bd612fc4ed7704b5
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-03-17 20:09:22 +0100

    working

--- test/GreedSpec.hs
+++ test/GreedSpec.hs
@@ -5,7 +5,6 @@ import Test.Hspec
 score :: [Int] -> Int
 score [1,1,1] = 1000
 score [a,b,c] | a == b && b == c = a * 100
-score [1,1] = 0
 score (1:rest) = 100 + score rest
 score [5] = 50
 score _ = 0
@@ -17,7 +16,7 @@ spec = describe "Greed" $ do
     it "A single one (100)" $ score [1] `shouldBe` 100
     it "A single five (50)" $ score [5] `shouldBe` 50
     it "A single two (0)" $ score [2] `shouldBe` 0
-    it "two 1s (0)" $ score [1,1] `shouldBe` 0
+    it "two 1s (0)" $ score [1,1] `shouldBe` 200
     it "triples 1 (1000)" $ score [1, 1, 1] `shouldBe` 1000
     it "triples 2 (200)" $ score [2, 2, 2] `shouldBe` 200
     it "triples 3 (300)" $ score [3,3,3] `shouldBe` 300
