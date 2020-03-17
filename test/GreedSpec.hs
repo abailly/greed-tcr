7e0fa1778c8c95c57ad9cd93ca0129f27e0ee644
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-03-17 20:07:40 +0100

    working

--- test/GreedSpec.hs
+++ test/GreedSpec.hs
@@ -4,9 +4,9 @@ import Test.Hspec
 
 score :: [Int] -> Int
 score [1,1,1] = 1000
-score [a,b, c] | a == b && b == c = a * 100
+score [a,b,c] | a == b && b == c = a * 100
 score [1,1] = 0
-score [1] = 100
+score (1:_) = 100
 score [5] = 50
 score _ = 0
 
@@ -14,23 +14,11 @@ score _ = 0
 spec :: Spec 
 spec = describe "Greed" $ do
 
-    it "A single one (100)" $ do
-        score [1] `shouldBe` 100
-
-    it "A single five (50)" $ do
-        score [5] `shouldBe` 50
-
-    it "A single two (0)" $ do
-        score [2] `shouldBe` 0
-
-    it "two 1s (0)" $ do
-        score [1,1] `shouldBe` 0
-    
-    it "triples 1 (1000)" $
-        score [1, 1, 1] `shouldBe` 1000
-
-    it "triples 2 (200)" $
-        score [2, 2, 2] `shouldBe` 200
-
-    it "triples 3 (300)" $
-        score [3,3,3] `shouldBe` 300
+    it "A single one (100)" $ score [1] `shouldBe` 100
+    it "A single five (50)" $ score [5] `shouldBe` 50
+    it "A single two (0)" $ score [2] `shouldBe` 0
+    it "two 1s (0)" $ score [1,1] `shouldBe` 0
+    it "triples 1 (1000)" $ score [1, 1, 1] `shouldBe` 1000
+    it "triples 2 (200)" $ score [2, 2, 2] `shouldBe` 200
+    it "triples 3 (300)" $ score [3,3,3] `shouldBe` 300
+    it "single 1 among others (100)" $ score [1,3,6] `shouldBe` 100
