05bba2443a607fbfaf5a3d6309e11004c45d3ce5
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-03-17 19:53:38 +0100

    working

--- test/GreedSpec.hs
+++ test/GreedSpec.hs
@@ -3,7 +3,8 @@ module GreedSpec where
 import Test.Hspec
 
 score :: [Int] -> Int
-score _ = 100
+score [1] = 100
+score [5] = 50
 
 -- http://codingdojo.org/kata/Greed/
 spec :: Spec 
@@ -11,3 +12,6 @@ spec = describe "Greed" $ do
 
     it "A single one (100)" $ do
         score [1] `shouldBe` 100
+
+    it "A single five (50)" $ do
+        score [5] `shouldBe` 50
